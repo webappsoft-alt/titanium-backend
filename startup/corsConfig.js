ce39158a12303d30785fd13e2d124766f49e4443
Atif31mirza <matif7909@gmail.com>
2026-01-13 16:48:07 +0500

    add-dev-cors

--- startup/corsConfig.js
+++ startup/corsConfig.js
@@ -8,6 +8,7 @@ const allowedOrigins = [
     'https://www.api.titanium.com',
     'https://titanium.com',
     'https://www.titanium.com',
+    'http://localhost:5004'
 ];
 const blockApiCallers = (req, res, next) => {
 
