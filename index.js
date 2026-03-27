ccdbdcf0db0db6771e2eb22ecda55ceab4b03469
Atif31mirza <matif7909@gmail.com>
2026-03-27 14:42:04 +0500

    remove-firebase

--- index.js
+++ index.js
@@ -11,25 +11,6 @@ const server = http.createServer(app);
 const auth = require('./middleware/auth')
 const optionalAuth = require('./middleware/optionalAuth')
 
-const admin = require("firebase-admin");
-const config = {
-  "type": process.env.TYPE,
-  "project_id": process.env.PROJECTID,
-  "private_key_id": process.env.PRIVATE_KEY_ID,
-  "private_key": process.env.PRIVATE_KEY,
-  "client_email": process.env.CLIENT_EMAIL,
-  "client_id": process.env.CLIENTID,
-  "auth_uri": process.env.AUTH_URI,
-  "token_uri": process.env.TOKEN_URL,
-  "auth_provider_x509_cert_url": process.env.AUTHPROVIDER,
-  "client_x509_cert_url": process.env.CLIENT_CERT,
-  "universe_domain": process.env.DOMAIN
-};
-
-admin.initializeApp({
-  credential: admin.credential.cert(config),
-  storageBucket: "gs://task-connect-app.appspot.com"
-});
 // ⚠️ Must call corsConfig before any routes
 require('./startup/corsConfig')(app);
 // 🔒 Security middleware (rate limiting, NoSQL sanitize, XSS, HPP)
